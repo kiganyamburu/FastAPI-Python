3cfcc341db29c0e02812a008f182c1d3db03cade
kiganyamburu <mburukiganya@gmail.com>
2024-05-23 19:56:14 +0300

    Read task

--- main.py
+++ main.py
@@ -2,12 +2,29 @@
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+from typing import list, Optional
+from uuid import UUID, uuid4
 
 app = FastAPI()
 
+class Task(BaseModel):
+    id:Optional[UUID] = None
+    title: str
+    description:Optional[str] = None
+    completed: bool = True
+
+task = []
+
+@pp.post("/tasks/", response_model=Task)
+def create_task(task: Task):
+    task.id = uuid4()
+    tasks.append(task)
+    return task
+
 # basic root 
-@app.get("/")
-def read():
+@app.get("/tasks/", response_model=list[Task])
+def read_tasks():
     return { "hello": "world!"}
 
 
